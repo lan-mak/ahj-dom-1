84c9d8e71803c9111f8b8e4feddd77717fdaca93
lan-mak <lan-mak@ya.ru>
2022-07-06 22:12:49 +0300

    init env

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// TODO: write code here
+const a = 5;
+const b = 16;
+const x = a + b;
+console.log(x);
---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  // Set the mode to development or production
+  mode: 'development',
+  // Control how source maps are generated
+  devtool: 'inline-source-map',
+
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    compress: true,
+    port: 9000,
+  },
+
+  plugins: [
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+});
