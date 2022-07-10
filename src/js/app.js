8ded3f819de845b89b2bdad2d286f15d3578879d
lan-mak <lan-mak@ya.ru>
2022-07-11 00:43:35 +0300

    ver 0.01

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,5 @@
 // TODO: write code here
-const fields = document.getElementsByClassName('field');
-const goblin = '<img src="https://raw.githubusercontent.com/lan-mak/ahj-homeworks/simplification/dom/pic/goblin.png" alt="Гоблин">';
+import Game from './Game';
 
-function randomImg() {
-  for (const i of fields) {
-    if (i.hasChildNodes()) {
-      i.querySelector('img').remove();
-    }
-  }
-
-  const randomIndex = Math.floor(Math.random() * fields.length);
-  fields[randomIndex].insertAdjacentHTML('afterbegin', goblin);
-}
-
-setInterval(randomImg, 1000);
+const newGame = new Game();
+newGame.init();
